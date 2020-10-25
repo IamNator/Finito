12de7cbad9408cb410cf5a43001823972888f039
Nator Verinumbe <natverior1@gmail.com>
2020-10-25 23:53:46 +0100

    Finito main app sending request to cloud

--- Hardware/Firmware/TEST1/SIM800/SIM800/SIM800.ino
+++ Hardware/Firmware/TEST1/SIM800/SIM800/SIM800.ino
@@ -1,44 +1,68 @@
-//SoftwareSerial mySerial(3, 2);
-#include "Wire.h"
-#include "SSD1306.h"  //For display
+#include <WiFi.h>
+#include <ESPmDNS.h>
+#include <WiFiUdp.h>
+#include <ArduinoOTA.h>
 
-SSD1306 display(0x3C, 5, 4); // display(I2C Address, SDA_Pin, SCL_Pin)
+const char* ssid = "Opeyemi";
+const char* password = "peaceunity";
 
-void DisplayResponse(String strings){
-  display.clear();
-  display.setFont(ArialMT_Plain_16);
-  display.setColor(WHITE);
-  display.setTextAlignment(TEXT_ALIGN_LEFT);
-  display.drawString(16, 8, "here" + strings );
-  display.display();
+void setup() {
+  Serial.begin(115200);
+  Serial.println("Booting");
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  while (WiFi.waitForConnectResult() != WL_CONNECTED) {
+    Serial.println("Connection Failed! Rebooting...");
+    delay(5000);
+    ESP.restart();
+  }
+
+  // Port defaults to 3232
+  // ArduinoOTA.setPort(3232);
+
+  // Hostname defaults to esp3232-[MAC]
+  // ArduinoOTA.setHostname("myesp32");
+
+  // No authentication by default
+  // ArduinoOTA.setPassword("admin");
+
+  // Password can be set with it's md5 value as well
+  // MD5(admin) = 21232f297a57a5a743894a0e4a801fc3
+  // ArduinoOTA.setPasswordHash("21232f297a57a5a743894a0e4a801fc3");
+
+  ArduinoOTA
+    .onStart([]() {
+      String type;
+      if (ArduinoOTA.getCommand() == U_FLASH)
+        type = "sketch";
+      else // U_SPIFFS
+        type = "filesystem";
+
+      // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
+      Serial.println("Start updating " + type);
+    })
+    .onEnd([]() {
+      Serial.println("\nEnd");
+    })
+    .onProgress([](unsigned int progress, unsigned int total) {
+      Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+    })
+    .onError([](ota_error_t error) {
+      Serial.printf("Error[%u]: ", error);
+      if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+      else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+      else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+      else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+      else if (error == OTA_END_ERROR) Serial.println("End Failed");
+    });
+
+  ArduinoOTA.begin();
+
+  Serial.println("Ready");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
 }
 
-void setup()
-{
-
-  char *c = "";
-  display.init();
-   Serial.begin(9600);
-  delay(1000);
-  Serial.println("AT");
-  while(Serial.available()){
-    c += Serial.read();
-  }
-  delay(1000);
-  DisplayResponse(String(c));
-  delay(500);
-  Serial.println("AT+CMGF=1");
-  delay(500);
-  Serial.println("AT+CMGS=\"+2349045057268\"\r");
-  delay(500);
-  Serial.print("Hello World from Dev Craze!");
-  delay(500);
-  Serial.write(26);
-}
- 
-void loop()
-{
-  while(Serial.available()){
-    Serial.println(char(Serial.read()));
-  }
+void loop() {
+  ArduinoOTA.handle();
 }
--- Hardware/Firmware/TEST1/wifiClient/wifiClient.ino
+++ Hardware/Firmware/TEST1/wifiClient/wifiClient.ino
@@ -9,16 +9,16 @@
 const char* ssid = "Opeyemi";
 const char* password = "peaceunity";
 
+String fname = "Nator"
+String lname = "Paul"
+
 //Your IP address or domain name with URL path
 const char* getTransactionToken  = "http://192.168.4.1/getTransactionToken";
 SSD1306 display(0x3C, 5, 4); // display(I2C Address, SDA_Pin, SCL_Pin)
 
 
 /*********Function Prototypes**************************/
-void DisplayConnecting();
-void DisplayDiscounted();
-void DisplayConnected(String ip_address);
-void DisplayTransactionDone(String *transactionToken);
+
 String httpGETRequest(const char* serverName);
 
 /****************Time***********************/
@@ -28,52 +28,6 @@ int transactionTokenNotSent = 1;
 String transactionToken;
 
 
-
-void setup() {
-
-  Serial.begin(115200); //For debuging through Serial monitor
-  display.init();
-
-  WiFi.begin(ssid, password);
-  Serial.println("Connecting");
-  
-  while(WiFi.status() != WL_CONNECTED) { 
-    DisplayConnecting();
-    delay(500);
-    Serial.println("Connecting...");
-  }
-  Serial.println("");
-  Serial.print("Connected to WiFi network with IP Address: ");
-  Serial.println(WiFi.localIP());
-  DisplayConnected(String(WiFi.localIP()));
-  
-}
-
-void loop() {
-
-  unsigned long currentMillis = millis();
-  
-  
-  if(currentMillis - previousMillis >= interval) {
-     // Check WiFi connection status
-    if(WiFi.status()== WL_CONNECTED ){ 
-      if (transactionTokenNotSent){
-          transactionToken = httpGETRequest(getTransactionToken);
-          Serial.println(transactionToken);
-          transactionTokenNotSent = 0;
-      }
-
-       //DisplayTransactionDone(&transactionToken);
-       DisplayConnected(String(WiFi.localIP()));
-       previousMillis = currentMillis;
-    }
-    else {
-      Serial.println("Wifi Disconnected");
-      DisplayDiscounted();
-      }
-    }
-  
-}
 
 
 String httpGETRequest(const char* serverName) {
@@ -104,39 +58,113 @@ String httpGETRequest(const char* serverName) {
 }
 
 
-void DisplayConnecting(){
+void DisplayUserDetails(){
   display.clear();
   display.setFont(ArialMT_Plain_16);
   display.setColor(WHITE);
   display.setTextAlignment(TEXT_ALIGN_LEFT);
-  display.drawString(16, 8, "Connecting..." );
+  display.drawString(16, 8, fname + " \n" + accountNumber );
   display.display();
 }
 
-void DisplayDiscounted(){
+void DisplayReadyToReceive(){
   display.clear();
   display.setFont(ArialMT_Plain_16);
   display.setColor(WHITE);
   display.setTextAlignment(TEXT_ALIGN_LEFT);
-  display.drawString(16, 8, "Disconnected..." );
+  display.drawString(16, 8, "Ready \nTo \nReceive" );
   display.display();
 }
 
-void DisplayConnected(String ip_address){
-  display.clear();
-  display.setFont(ArialMT_Plain_10);
-  display.setColor(WHITE);
-  display.setTextAlignment(TEXT_ALIGN_LEFT);
-  display.drawString(0, 0, "Connected to \n"+ip_address );
-  display.display();
+void DisplayTransactionReceived(String *transactionToken){
+    DeserializationError error = deserializeJson(doc, *transactionToken);
+    //Test if parsing succeeds.
+    if (error) {
+      Serial.print(F("deserializeJson() failed: "));
+      Serial.println(error.f_str());
+      return;
+    }
+
+    // Most of the time, you can rely on the implicit casts.
+    // In other case, you can do doc["time"].as<long>();
+    String Amount = doc["amount"];
+    String  fname = doc["fname"];
+    String lname = doc["lname"];
+
+    display.clear();
+    display.setFont(ArialMT_Plain_10);
+    display.setColor(WHITE);
+    display.setTextAlignment(TEXT_ALIGN_LEFT);
+    //transactionToken
+    display.drawString(16, 8, Amount + " received \nfrom "+fname +" " + lname );
+    display.display();
 }
 
-void DisplayTransactionDone(String *transactionToken){
-  display.clear();
-  display.setFont(ArialMT_Plain_16);
-  display.setColor(WHITE);
-  display.setTextAlignment(TEXT_ALIGN_LEFT);
-  //transactionToken
-  display.drawString(16, 8, "Token Received" );
-  display.display();
+void DisplayPressOneToContinue(){
+    display.clear();
+    display.setFont(ArialMT_Plain_16);
+    display.setColor(WHITE);
+    display.setTextAlignment(TEXT_ALIGN_LEFT);
+    display.drawString(16, 8, "Press one \nTo \nContinue" );
+    display.display();
+}
+
+void DisplayFinito(){
+    display.clear();
+    display.setFont(ArialMT_Plain_24);
+    display.setColor(WHITE);
+    display.setTextAlignment(TEXT_ALIGN_LEFT);
+    //transactionToken
+    display.drawString(16, 8, "Finito" );
+    display.display();
+}
+
+
+
+void setup() {
+
+  Serial.begin(115200); //For debuging through Serial monitor
+  display.init();
+
+  WiFi.begin(ssid, password);
+  Serial.println("Connecting");
+  
+  while(WiFi.status() != WL_CONNECTED) { 
+    DisplayConnecting();
+    delay(500);
+    Serial.println("Connecting...");
+    DisplayUserDetails();
+  }
+  
+  Serial.println("");
+  Serial.print("Connected to WiFi network with IP Address: ");
+  Serial.println(WiFi.localIP());
+  DisplayReadyToReceive();
+  
+}
+
+void loop() {
+
+  unsigned long currentMillis = millis();
+  
+  
+  if(currentMillis - previousMillis >= interval) {
+     // Check WiFi connection status
+    if(WiFi.status()== WL_CONNECTED ){ 
+      if (transactionTokenNotSent){
+          transactionToken = httpGETRequest(getTransactionToken);
+          Serial.println(transactionToken);
+          transactionTokenNotSent = 0;
+      }
+
+       //DisplayTransactionDone(&transactionToken);
+       DisplayConnected(String(WiFi.localIP()));
+       previousMillis = currentMillis;
+    }
+    else {
+      Serial.println("Wifi Disconnected");
+      DisplayDiscounted();
+      }
+    }
+  
 }
